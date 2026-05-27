bfb8fdd23b50e7b80262cdfc10ea48b605d222fa
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-05-27 22:52:41 +0400

    Add files via upload

---
+++ day 004 homework.py
@@ -0,0 +1,11 @@
+
+# name_of_variable = "hello"
+# price_of_candy = 1
+# firstName = "giorgi" 
+# lastName = "qvaraia"
+
+
+name = input("enter your name:")
+print(name)
+age = input("enter your age:")
+print("you are " + age + " years old.")
