f90877cfcc5f9dc3a55ae581cfefa5b61dff1b5c
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-05-27 22:50:13 +0400

    Add files via upload

---
+++ day 002 homework.py
@@ -0,0 +1,25 @@
+print ("giorgi")
+print ("saba")
+print (50)
+print (10)
+print (0.8)
+print (0.5)
+print (False)
+print (True) 
+
+
+
+
+
+
+#konkatenacia aris string+string magalitad gamarjoba+naxvamdis
+#debbuging aris bugis anu kodis shecdomis gasworeba
+name='giorgi'
+print (name)
+variable1=10
+variable2=20
+variable3=30
+print (variable1*variable2*variable3)
+length=40
+width=30
+print (length+width)
