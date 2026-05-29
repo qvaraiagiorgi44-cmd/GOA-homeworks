e9ef7fdfca9f1f0624fde23ae9bcd193e4d9c5ce
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-05-29 23:03:35 +0400

    Delete day 1 homework.py

--- day 1 homework.py
+++
@@ -1,9 +0,0 @@
-print ("komentari sheidzleba gamogvadges rodesac vaketebt did proekts da gvwirdeba rom gavixsenot raze vmusaobdit bolos")
-print ("sushi")
-print ("xinkali")
-print ("khachapuri")
-print ("pizza")
-print ("mwvadi")
-name="giorgi"
-lastname="qvaraia"
-age="11"
