e5039f8a70a281984239990c238e97d71bbc324d
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-05-30 23:25:50 +0400

    Add files via upload

---
+++ day 006/classwork/day 006 classwork.py
@@ -0,0 +1,17 @@
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+print("your name is " + name + " and you are " + age + " years old")
+
+
+# 4) ახსენით რას აკეთებს type() ფუნქცია.
+# 5) დაწერე კოდი რომელიც შეამოწმებს ჩვენთვის ნაცნობ 4 მონაცემთა ტიპს და დაპრინტეთ შედეგი.
+
+#type funqcia gvacvenebs monacemis tips
+
+
+
+print(type ("hello world"))
+print(type (5))
+print(type (3.14))
+print(type (True))
