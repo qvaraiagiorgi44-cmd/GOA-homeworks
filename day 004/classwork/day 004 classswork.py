4b55f48a2d3eb185a5213bd08825e2d411e6501c
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-05-30 23:24:58 +0400

    Add files via upload

---
+++ day 004/classwork/day 004 classswork.py
@@ -0,0 +1,10 @@
+#1) შექმენით 2 ცვლადი snake_caseის გამოყენებით
+#2) შექმენით 2 ცვლადი camelCaseის გამოყენებით
+
+name_of_variable = "hello"
+price_of_candy = 1
+firstName = "gega" 
+lastName = "pirtskhelava"
+
+
+
