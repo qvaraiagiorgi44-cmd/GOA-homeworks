1a6ec227bfc3c76410ad798004f52920ec56ebce
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-05-30 23:23:22 +0400

    Add files via upload

---
+++ day 001/home work/day 1 homework.py
@@ -0,0 +1,9 @@
+print ("komentari sheidzleba gamogvadges rodesac vaketebt did proekts da gvwirdeba rom gavixsenot raze vmusaobdit bolos")
+print ("sushi")
+print ("xinkali")
+print ("khachapuri")
+print ("pizza")
+print ("mwvadi")
+name="giorgi"
+lastname="qvaraia"
+age="11"
