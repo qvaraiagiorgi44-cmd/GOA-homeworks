1d8feba99149ce5ca22f645b3aa08a465c1400fd
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-06-27 19:36:49 +0400

    Add files via upload

---
+++ day 14/day 14 homework/day 14 homework.py
@@ -0,0 +1,25 @@
+
+#sequence kodi sruldeba zemodan qvemot rigrigobit
+#selection programa amowmebs pirobas da imis mixedvit asrulebs ert-ert nawils
+#iteration erti da igive kodi meordeba ramdenjerme
+
+#i = 1 bewdavs 1 i=2 bedwavs 2 da ase semdeg
+
+for i in range(1, 1001):
+    print(i)
+
+for i in range(1, 16):
+    print("სუში")
+
+for i in range(1, 6):
+    print("ia")
+
+for i in range(1, 6):
+    print("leri")
+
+for i in range(1, 6):
+    print("aleqsandre")
+
+for i in range(1, 6):
+    print("anita")
+ 
