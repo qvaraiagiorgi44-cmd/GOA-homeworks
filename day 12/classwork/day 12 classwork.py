21874e4859120404519fc7ff44f797550e7e62d5
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-06-27 17:39:12 +0400

    Add files via upload

---
+++ day 12/classwork/day 12 classwork.py
@@ -0,0 +1,11 @@
+#  ლოგიკური ოპერატორები: and or. or aris rodesac or magalits dawer da erti mainc tu iqneba true masin trues gamoitans
+# xolo and aris rodesac magalits dawer da erti mainc tu iqneba false masin false gamoitans
+
+#  es kodi gamoitans false radgan aris and roelshic erti mainc tu iqneba false mashin false gamoaqvs
+
+#
+print  (100 > 50) #True
+print  (50 < 100)  #True
+print  (50 == 50)  #True
+print (50 != 100)  #false
+print (100 <= 50)  #false
