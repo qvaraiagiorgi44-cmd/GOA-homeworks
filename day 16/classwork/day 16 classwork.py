79c396a28bc602143ed9c42fac014ea1efaaf217
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-06-27 19:03:02 +0400

    Add files via upload

---
+++ day 16/classwork/day 16 classwork.py
@@ -0,0 +1,21 @@
+for i in range(1, 101):
+    print("BMW M5")
+
+
+for i in range(1, 101):
+    print("leri")
+
+for i in range(1, 101):
+    print("ia")
+
+for i in range(1, 51):
+    print("anita")
+
+for i in range(1, 51):
+    print("aleqsandre") 
+
+for i in range(1, 1000001):
+    print  (i)
+
+for i in range(1, 51):
+    print("hello world")
---
+++ day 16/homework/day 16 homework.py
@@ -0,0 +1,22 @@
+x = 1
+while x <= 20:
+    print(x)
+    x *= 2
+
+counter = 5
+while counter <= 20:
+    print(counter)
+    counter += 3  
+
+
+sum = 0
+for i in range(1, 6):
+    sum += i
+print(sum)
+
+
+
+product = 1
+for i in range(1, 5):
+    product *= i
+print(product)
