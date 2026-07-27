985e2aa385c3a2076f0aecc32ef0d04471f129ca
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-07-27 22:12:58 +0400

    Add files via upload

---
+++ day 25/classwork/day 25 classwork.py
@@ -0,0 +1,18 @@
+# 1) შექმენით 1 სია და  შეინახეთ ოჯახის წევრების სახელები, შემდგომ დაპრინტეთ ეს სია.
+
+# 2) შექმენით სია fruits, სადაც შეინახავ რამოდენიმე ხილს და შემდეგ ტერმინალში გამოიტანე ასეთი წინადადება: I love (ხილი) - მაგრამ ის ხილი რომელიც თქვენ გიყვართ (გამოიყენეთ ინდექსინგი)
+
+# 3) კომენტარებით ახსეით რა არის mutable და immutable მონაცემთა ტიპები
+
+
+names = ["ia", "leri", "giorgi", "anita", "alexandre"]
+print (names)
+
+fruits = ["banana", "apple", "watermelon", "strawberry"]
+print ("i love", fruits[2])
+
+# mutable - monacemi romlis secvla seizleba seqmnis semdeg
+# immutable monacemi romlis secvla pirdapir ar seizleba
+
+# sia monacemta tipia romelic inaxavs ramdenime mnisvnelobas ert cvladsi
+# indexing siasi romelime mnisvnelobaze wvdoma misi nomrit
