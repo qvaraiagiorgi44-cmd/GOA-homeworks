a185c95a7b1fe0a61495a8f247cd36db75e2ede1
qvaraiagiorgi44-cmd <qvaraiagiorgi44@gmail.com>
2026-08-29 14:54:12 +0400

    Add files via upload

---
+++ day 29/classwork/day 29 classwork.py
@@ -0,0 +1,21 @@
+# 1) მოიყვანეთ თითოეულ ნასწავლ ფუნქციაზე ერთი მაგალითი
+print("cool guy".upper())
+
+print("cool guy".lower())
+
+print("cool guy".capitalize())
+
+print("cool guy".title())
+
+print("cool guy".strip())
+
+print("cool guy".rstrip())
+
+print("cool guy".lstrip())
+
+print("cool guy".replace("guy", "girl"))
+
+print("cool guy".find("y"))
+
+text = "goa"
+print(text.count("a"))
---
+++ day 29/homework/day 29 homework.py
@@ -0,0 +1,37 @@
+# 1)
+# lower yvela asos aqcevs patara asod
+
+# capitalize pirvel asos adidebs
+
+# title yvela sityvis pirvel asos adidebs
+
+# strip amatebs spaces marjvniv da marcxniv
+
+# rstrip amatebs spaces marjvniv
+
+# lstrip amatebs spaces marcxniv
+
+# replace stringshi cvlis romelime sityas sxva sityvit
+
+# find poulobs romelime asos sityvasi
+
+# count itvlis romelime asos sityvasi
+
+#2)
+# split - strings yofs nawilebad
+# join - aertianebs
+# start with amowmebs tu am sityvit iwyeba
+# endswith amowmebs tu am sityvit mtavrdeba
+
+#3)
+fullname = input("შეიყვანე შენი სახელი და გვარი: ")
+print(fullname.title())
+
+
+#4)
+name = input("enter your name: ")
+
+if name[0].upper():
+    print(name.upper())
+else:
+    print(name.title())
